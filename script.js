99abe8877e94d1d219f8f7bb2e539c3389adab33
programmertaha <programmer.taha.1y@gmail.com>
2020-09-04 19:27:38 +0300

    initial comment and code

---
+++ script.js
@@ -0,0 +1,55 @@
+/*
+
+service called Training Days
+
+% sends you a message for the event you signed up for and the days you have left to train.
+
+todo: you will make Training Days more maintainable and less error-prone by fixing variable scopes.
+
+*/
+
+// The scope of `random` is too loose 
+const random = Math.floor(Math.random() * 3);
+
+const getRandEvent = () => {
+  if (random === 0) {
+    return 'Marathon';
+  } else if (random === 1) {
+    return 'Triathlon';
+  } else if (random === 2) {
+    return 'Pentathlon';
+  }
+};
+
+// The scope of `days` is too tight 
+const getTrainingDays = event => {
+
+  if (event === 'Marathon') {
+    let days = 50;
+  } else if (event === 'Triathlon') {
+    let days = 100;
+  } else if (event === 'Pentathlon') {
+    let days = 200;
+  }
+
+  return days;
+};
+
+// The scope of `name` is too tight 
+const logEvent = event => {
+  const name = 'Nala';
+  console.log(`${name}'s event is: ${event}`);
+};
+
+const logTime = days => {
+  const name = 'Nala';
+  console.log(`${name}'s time to train is: ${days} days`);
+};
+
+const event = getRandEvent();
+const days = getTrainingDays(event);
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
+
+
+logEvent(event);
+logTime(days);
